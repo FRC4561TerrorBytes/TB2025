c2ba8129f9dc8e74a5960e49e9a7dc3ba1dea7e1
Manbir Guron <mnguron19@gmail.com>
2025-02-15 15:21:27 -0500

    fix: Whitespace

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -31,7 +31,8 @@ public class Intake extends SubsystemBase {
   }
 
   public Command intakeCoral() {
-    return new RunCommand(() -> this.setOutput(0.75), this).until(() -> inputs.intakeLimitSwitch);
+    return new RunCommand(
+      () -> this.setOutput(0.75), this).until(() -> inputs.intakeLimitSwitch);
   }
 
   public Command outtakeCoral() {
