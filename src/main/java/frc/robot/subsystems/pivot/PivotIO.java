ab44eab54410ba632955388ae01789a8f54c48b7
tyler <ninjanarwhal07@gmail.com>
2025-02-18 19:09:50 -0500

    these werent staged

---
+++ src/main/java/frc/robot/subsystems/pivot/PivotIO.java
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.pivot;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface PivotIO {
+
+  @AutoLog
+  public static class PivotIOInputs {
+    public double pivotAngle = 0.0;
+    public double extensionHeight = 0.0;
+    // pivot components
+    public double pivotStatorCurrent = 0.0;
+    public double pivotSupplyCurrent = 0.0;
+    public double pivotVoltage = 0.0;
+    public double pivotSetpoint = 0.0;
+    public double pivotMotorOneTemp = 0.0;
+    public boolean pivotMotorOneConnected = false;
+    public double pivotMotorTwoTemp = 0.0;
+    public boolean pivotMotorTwoConnected = false;
+    public double pivotMotorThreeTemp = 0.0;
+    public boolean pivotMotorThreeConnected = false;
+    public double pivotMotorFourTemp = 0.0;
+    public boolean pivotMotorFourConnected = false;
+    public double pivotSpeed = 0.0;
+    // carriage components
+    public double extensionStatorCurrent = 0.0;
+    public double extensionSupplyCurrent = 0.0;
+    public double extensionVoltage = 0.0;
+    public double extensionSetpoint = 0.0;
+    public double extensionMotorTemp = 0.0;
+    public boolean extensionMotorConnected = false;
+    public double extensionSpeed = 0.0;
+  }
+
+  /** Updates the set of loggable inputs. */
+  public default void updateInputs(PivotIOInputs inputs) {}
+
+  public default void setExtensionPosition(double position) {}
+
+  public default void setPivotPosition(double position) {}
+
+  public default void setExtensionVoltage(double voltage) {}
+
+  public default void setPivotVoltage(double voltage) {}
+}
---
+++ src/main/java/frc/robot/subsystems/pivot/PivotIOReal.java
@@ -0,0 +1,201 @@
+package frc.robot.subsystems.pivot;
+
+import static frc.robot.util.PhoenixUtil.tryUntilOk;
+
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.ParentDevice;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.GravityTypeValue;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Temperature;
+import edu.wpi.first.units.measure.Voltage;
+import frc.robot.Constants;
+
+public class PivotIOReal implements PivotIO {
+
+  private TalonFX pivotMotorL1 = new TalonFX(Constants.LEFT_PIVOT_ID_1);
+  private TalonFX pivotMotorL2 = new TalonFX(Constants.LEFT_PIVOT_ID_2);
+  private TalonFX pivotMotorR1 = new TalonFX(Constants.RIGHT_PIVOT_ID_1);
+  private TalonFX pivotMotorR2 = new TalonFX(Constants.RIGHT_PIVOT_ID_2);
+
+  private CANcoder pivotEncoder = new CANcoder(30);
+
+  private TalonFX extensionMotor = new TalonFX(Constants.EXTENSION_ID);
+
+  private final StatusSignal<Angle> pivotAngle;
+  private final StatusSignal<Current> pivotStatorCurrent;
+  private final StatusSignal<Current> pivotSupplyCurrent;
+  private final StatusSignal<AngularVelocity> pivotSpeed;
+  private final StatusSignal<Voltage> pivotVoltage;
+  private final StatusSignal<Temperature> pivotOneTemp;
+
+  private final StatusSignal<Angle> extensionHeight;
+  private final StatusSignal<Current> extensionStatorCurrent;
+  private final StatusSignal<Current> extensionSupplyCurrent;
+  private final StatusSignal<AngularVelocity> extensionSpeed;
+  private final StatusSignal<Voltage> extensionVoltage;
+  private final StatusSignal<Temperature> extensionTemp;
+
+  private double pivotSetpoint = 0.0;
+  private double pivotFeedForward = 0.0;
+  private double extensionSetpoint = 0.0;
+  private double extensionFeedForward = 0.0;
+
+  private final MotionMagicVoltage m_request_pivot = new MotionMagicVoltage(0);
+  private final MotionMagicVoltage m_request_extension = new MotionMagicVoltage(0);
+
+  public PivotIOReal() {
+    var pivotPIDConfig = new Slot0Configs();
+    pivotPIDConfig.GravityType = GravityTypeValue.Arm_Cosine;
+    // pivotPIDConfig.kS = 0.28;
+    pivotPIDConfig.kV = 0;
+    pivotPIDConfig.kA = 0;
+    pivotPIDConfig.kP = 4;
+    pivotPIDConfig.kI = 0;
+    pivotPIDConfig.kD = 0;
+
+    var pivotConfig = new TalonFXConfiguration();
+    pivotConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+    pivotConfig.Slot0 = pivotPIDConfig; //
+    pivotConfig.Feedback.SensorToMechanismRatio = Constants.PIVOT_GEAR_RATIO;
+    pivotConfig.MotionMagic.MotionMagicCruiseVelocity = 100.0 / Constants.PIVOT_GEAR_RATIO;
+    pivotConfig.MotionMagic.MotionMagicAcceleration =
+        pivotConfig.MotionMagic.MotionMagicCruiseVelocity / 0.100;
+    pivotConfig.MotionMagic.MotionMagicExpo_kV = 0.12 * Constants.PIVOT_GEAR_RATIO;
+    pivotConfig.MotionMagic.MotionMagicExpo_kA = 0.1;
+    pivotConfig.ClosedLoopGeneral.ContinuousWrap = false;
+    pivotConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+    pivotConfig.SoftwareLimitSwitch.ReverseSoftLimitThreshold = 0.28;
+    pivotConfig.SoftwareLimitSwitch.ForwardSoftLimitThreshold = 0.4;
+    pivotConfig.SoftwareLimitSwitch.ReverseSoftLimitEnable = true;
+    pivotConfig.SoftwareLimitSwitch.ForwardSoftLimitEnable = false;
+    tryUntilOk(5, () -> pivotMotorL1.getConfigurator().apply(pivotConfig, 0.25));
+
+    pivotMotorL1.setPosition(-0.01025390625 + 0.003173828125 + 0.000244140625);
+    pivotMotorL1.setPosition(pivotEncoder.getAbsolutePosition().getValueAsDouble());
+
+    pivotAngle = pivotEncoder.getAbsolutePosition();
+    pivotStatorCurrent = pivotMotorL1.getStatorCurrent();
+    pivotSupplyCurrent = pivotMotorL1.getSupplyCurrent();
+    pivotSpeed = pivotMotorL1.getVelocity();
+    pivotVoltage = pivotMotorL1.getMotorVoltage();
+    pivotOneTemp = pivotMotorL1.getDeviceTemp();
+
+    var extensionPIDConfig = new Slot0Configs();
+    extensionPIDConfig.GravityType = GravityTypeValue.Arm_Cosine;
+    // pivotPIDConfig.kS = 0.28;
+    extensionPIDConfig.kV = 0;
+    extensionPIDConfig.kA = 0;
+    extensionPIDConfig.kP = 0.5;
+    extensionPIDConfig.kI = 0;
+    extensionPIDConfig.kD = 0;
+
+    var extensionConfig = new TalonFXConfiguration();
+    extensionConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+    extensionConfig.Slot0 = extensionPIDConfig; //
+    extensionConfig.Feedback.SensorToMechanismRatio = Constants.PIVOT_GEAR_RATIO;
+    extensionConfig.MotionMagic.MotionMagicCruiseVelocity = 100.0 / Constants.PIVOT_GEAR_RATIO;
+    extensionConfig.MotionMagic.MotionMagicAcceleration =
+        extensionConfig.MotionMagic.MotionMagicCruiseVelocity / 0.100;
+    extensionConfig.MotionMagic.MotionMagicExpo_kV = 0.12 * Constants.PIVOT_GEAR_RATIO;
+    extensionConfig.MotionMagic.MotionMagicExpo_kA = 0.1;
+    extensionConfig.ClosedLoopGeneral.ContinuousWrap = false;
+    extensionConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+    extensionConfig.SoftwareLimitSwitch.ReverseSoftLimitThreshold = 0.004;
+    extensionConfig.SoftwareLimitSwitch.ForwardSoftLimitThreshold = 0.08;
+    extensionConfig.SoftwareLimitSwitch.ReverseSoftLimitEnable = true;
+    extensionConfig.SoftwareLimitSwitch.ForwardSoftLimitEnable = true;
+    tryUntilOk(5, () -> extensionMotor.getConfigurator().apply(extensionConfig, 0.25));
+
+    extensionMotor.setPosition(0);
+
+    extensionHeight = extensionMotor.getPosition();
+    extensionStatorCurrent = extensionMotor.getStatorCurrent();
+    extensionSupplyCurrent = extensionMotor.getSupplyCurrent();
+    extensionSpeed = extensionMotor.getVelocity();
+    extensionVoltage = extensionMotor.getMotorVoltage();
+    extensionTemp = extensionMotor.getDeviceTemp();
+
+    BaseStatusSignal.setUpdateFrequencyForAll(
+        50.0,
+        pivotAngle,
+        pivotStatorCurrent,
+        pivotSupplyCurrent,
+        pivotSpeed,
+        pivotVoltage,
+        pivotOneTemp,
+        extensionHeight,
+        extensionStatorCurrent,
+        extensionSupplyCurrent,
+        extensionSpeed,
+        extensionVoltage,
+        extensionTemp);
+
+    ParentDevice.optimizeBusUtilizationForAll(pivotMotorL1);
+  }
+
+  public void updateInputs(PivotIOInputs inputs) {
+    var pivotOneStatus =
+        BaseStatusSignal.refreshAll(
+            pivotAngle,
+            pivotStatorCurrent,
+            pivotSupplyCurrent,
+            pivotSpeed,
+            pivotVoltage,
+            pivotOneTemp,
+            extensionHeight,
+            extensionStatorCurrent,
+            extensionSupplyCurrent,
+            extensionSpeed,
+            extensionVoltage,
+            extensionTemp);
+
+    inputs.pivotAngle = pivotAngle.getValueAsDouble();
+    inputs.extensionHeight = extensionHeight.getValueAsDouble();
+
+    inputs.extensionMotorConnected = pivotOneStatus.isOK();
+    inputs.extensionStatorCurrent = pivotStatorCurrent.getValueAsDouble();
+    inputs.extensionSupplyCurrent = pivotSupplyCurrent.getValueAsDouble();
+    inputs.extensionSpeed = pivotMotorL1.getVelocity().getValueAsDouble();
+    inputs.extensionVoltage = pivotMotorL1.getMotorVoltage().getValueAsDouble();
+    inputs.extensionSetpoint = this.pivotSetpoint;
+
+    inputs.pivotMotorOneConnected = pivotOneStatus.isOK();
+    inputs.pivotStatorCurrent = pivotStatorCurrent.getValueAsDouble();
+    inputs.pivotSupplyCurrent = pivotSupplyCurrent.getValueAsDouble();
+    inputs.pivotSpeed = pivotMotorL1.getVelocity().getValueAsDouble();
+    inputs.pivotVoltage = pivotMotorL1.getMotorVoltage().getValueAsDouble();
+    inputs.pivotSetpoint = this.pivotSetpoint;
+
+    m_request_pivot.FeedForward =
+        Math.cos(Units.degreesToRadians(inputs.pivotAngle)) * pivotFeedForward;
+  }
+
+  public void setPivotPosition(double angle) {
+    pivotSetpoint = angle;
+    pivotMotorL1.setControl(m_request_pivot.withPosition(pivotSetpoint));
+  }
+
+  public void setPivotVoltage(double voltage) {
+    pivotMotorL1.setVoltage(voltage);
+  }
+
+  public void setExtensionPosition(double angle) {
+    extensionSetpoint = angle;
+    extensionMotor.setControl(m_request_extension.withPosition(extensionSetpoint));
+  }
+
+  public void setExtensionVoltage(double voltage) {
+    extensionMotor.setVoltage(voltage);
+  }
+}
