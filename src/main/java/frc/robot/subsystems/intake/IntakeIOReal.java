38f1628a6d5746f80a53e743ad11dc0ae027eb5e
Ananya <ananya.borkar18@gmail.com>
2025-10-16 18:52:16 -0400

    converted intake to kraken

--- src/main/java/frc/robot/subsystems/intake/IntakeIOReal.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOReal.java
@@ -3,8 +3,14 @@ package frc.robot.subsystems.intake;
 import static frc.robot.util.PhoenixUtil.tryUntilOk;
 import static frc.robot.util.SparkUtil.tryUntilOk;
 
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.CANrangeConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.CANrange;
+import com.ctre.phoenix6.hardware.ParentDevice;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.ctre.phoenix6.signals.UpdateModeValue;
 import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkBase.PersistMode;
@@ -14,23 +20,33 @@ import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Temperature;
+import edu.wpi.first.units.measure.Voltage;
 import frc.robot.Constants;
 
 public class IntakeIOReal implements IntakeIO {
-  private final SparkBase intakeMotor =
-      new SparkMax(Constants.INTAKE_MOTOR_ID, MotorType.kBrushless);
 
+
+      private TalonFX intakeMotor = new TalonFX(Constants.INTAKE_MOTOR_ID);
   private final CANrange canRange = new CANrange(Constants.CANRANGE_ID);
+  private final StatusSignal<Current> IntakeStatorCurrent;
+  private final StatusSignal<Current> IntakeSupplyCurrent;
+  private final StatusSignal<AngularVelocity> IntakeSpeed;
+  private final StatusSignal<Voltage> IntakeVoltage;
+  private final StatusSignal<Temperature> IntakeTemp;
 
   public IntakeIOReal() {
-    var intakeConfig = new SparkMaxConfig();
-
-    intakeConfig.idleMode(IdleMode.kBrake);
-    intakeConfig.voltageCompensation(12.0);
-    intakeConfig.inverted(false);
-    intakeConfig.smartCurrentLimit(Constants.INTAKE_CURRENT_LIMIT);
-    intakeConfig.limitSwitch.reverseLimitSwitchEnabled(false);
-    intakeConfig.limitSwitch.forwardLimitSwitchEnabled(false);
+    var intakeConfig = new TalonFXConfiguration();
+    intakeConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+    intakeConfig.CurrentLimits.StatorCurrentLimit = Constants.WRIST_STATOR_CURRENT_LIMIT;
+    intakeConfig.CurrentLimits.SupplyCurrentLimit = Constants.WRIST_SUPPLY_CURRENT_LIMIT;
+    intakeConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+    intakeConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+    tryUntilOk(5, () -> intakeMotor.getConfigurator().apply(intakeConfig, 0.25));
+    
 
     CANrangeConfiguration config = new CANrangeConfiguration();
     config.ProximityParams.MinSignalStrengthForValidMeasurement = 2000;
@@ -38,20 +54,38 @@ public class IntakeIOReal implements IntakeIO {
     config.ToFParams.UpdateMode = UpdateModeValue.ShortRange100Hz;
     tryUntilOk(5, () -> canRange.getConfigurator().apply(config, 0.25));
 
-    tryUntilOk(
-        intakeMotor,
-        5,
-        () ->
-            intakeMotor.configure(
-                intakeConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters));
+    IntakeStatorCurrent = intakeMotor.getStatorCurrent();
+    IntakeSupplyCurrent = intakeMotor.getSupplyCurrent();
+    IntakeSpeed = intakeMotor.getVelocity();
+    IntakeVoltage = intakeMotor.getMotorVoltage();
+    IntakeTemp = intakeMotor.getDeviceTemp();
+
+        BaseStatusSignal.setUpdateFrequencyForAll(
+        50.0,
+        IntakeStatorCurrent,
+        IntakeSupplyCurrent,
+        IntakeSpeed,
+        IntakeVoltage,
+        IntakeTemp);
+
+    ParentDevice.optimizeBusUtilizationForAll(intakeMotor, canRange);
+
+  
   }
 
   public void updateInputs(IntakeIOInputs inputs) {
+    var IntakeStatus =
+        BaseStatusSignal.refreshAll(
+            IntakeStatorCurrent,
+            IntakeSupplyCurrent,
+            IntakeSpeed,
+            IntakeVoltage,
+            IntakeTemp);
 
-    inputs.intakeVelocity = intakeMotor.getEncoder().getVelocity();
-    inputs.intakeCurrentAmps = intakeMotor.getOutputCurrent();
-    inputs.intakeVoltage = intakeMotor.getBusVoltage();
-    inputs.intakeConnected = !intakeMotor.hasActiveFault();
+    inputs.intakeVelocity = intakeMotor.getVelocity().getValueAsDouble();
+    inputs.intakeCurrentAmps = IntakeStatorCurrent.getValueAsDouble();
+    inputs.intakeVoltage = intakeMotor.getMotorVoltage().getValueAsDouble();
+    inputs.intakeConnected = IntakeStatus.isOK();
     inputs.coralPresent = canRange.getIsDetected().getValue();
     inputs.canRangeConnected = canRange.isConnected();
   }
